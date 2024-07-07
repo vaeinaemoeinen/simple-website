3ed302698a1eaf4306099eb5125fb1b4ea1c45fe
vaeinaemoeinen <phil.lentner@gmail.com>
2024-07-07 21:07:49 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 function searchList() {
-    var input, filter, ul, li, a, i, txtValue;
+    var input, filter, ul, li, a, i, txtValue, keywords;
     input = document.getElementById('searchInput');
     filter = input.value.toUpperCase();
     ul = document.getElementById('myList');
@@ -7,8 +7,9 @@ function searchList() {
 
     for (i = 0; i < li.length; i++) {
         a = li[i].getElementsByTagName('a')[0];
+        keywords = li[i].getAttribute('data-keywords');
         txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        if (txtValue.toUpperCase().indexOf(filter) > -1 || keywords.toUpperCase().indexOf(filter) > -1) {
             li[i].style.display = '';
         } else {
             li[i].style.display = 'none';
