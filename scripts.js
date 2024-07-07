aafca8eb45e7ce4d57501a064ec37e446f2d0c93
vaeinaemoeinen <phil.lentner@gmail.com>
2024-07-07 21:20:36 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,18 +1,24 @@
 function searchList() {
-    var input, filter, ul, li, a, i, txtValue, keywords;
+    var input, filter, list, divs, a, i, txtValue, keywords;
     input = document.getElementById('searchInput');
     filter = input.value.toUpperCase();
-    ul = document.getElementById('myList');
-    li = ul.getElementsByTagName('li');
+    list = document.getElementById('myList');
+    divs = list.getElementsByTagName('div');
 
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName('a')[0];
-        keywords = li[i].getAttribute('data-keywords');
+    if (filter === '') {
+        list.style.display = 'none'; // Hide the list if no input
+    } else {
+        list.style.display = 'block'; // Show the list if input is present
+    }
+
+    for (i = 0; i < divs.length; i++) {
+        a = divs[i].getElementsByTagName('a')[0];
+        keywords = divs[i].getAttribute('data-keywords');
         txtValue = a.textContent || a.innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1 || keywords.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = '';
+            divs[i].style.display = '';
         } else {
-            li[i].style.display = 'none';
+            divs[i].style.display = 'none';
         }
     }
 }
